70d148d252d6451cad96cfba4166cd054f78bf1f
halki <s.halki0203@icloud.com>
2021-06-08 21:53:56 +0900

    like, unlikeメソッド追加

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,13 @@ Auth::routes();
 Route::get('/','ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
 Route::resource('/articles', 'ArticleController')->only('show');
+
+Route::prefix('articles')->name('articles')->group(function(){
+    Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
+    Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
+
+});
+
+// prefixメソッド, nameメソッドによって下記の文が上記の様な記述に簡略化できる
+// Route::put('articles/{article}/like', 'ArticleController@like')->name('articles.like')->middleware('auth');
+// Route::delete('articles/{article}/like', 'ArticleController@unlike')->name('articles.unlike')->middleware('auth');
