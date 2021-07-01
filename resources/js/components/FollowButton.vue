4146dbb486ad2c12361e43d6f09376a2da1aab46
halki <s.halki0203@icloud.com>
2021-07-01 22:45:55 +0900

    フォロー機能実装

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -21,6 +21,13 @@ export default {
       type: Boolean,
       default: false,
     },
+    authorized: {
+      type: Boolean,
+      default: false,
+    },
+    endpoint: {
+      type: String
+    }
   },
   data(){
     return{
@@ -42,6 +49,28 @@ export default {
       return this.isFollowedBy
         ?'フォロー中'
         :'フォロー'
+    },
+  },
+  methods: {
+    clickFollow() {
+      if(!this.authorized) {
+        alert('フォロー機能はログイン中のみ使用できます')
+        return
+      }
+
+      this.isFollowedBy
+        ? this.unfollow()
+        : this.follow()
+    },
+    async follow() {
+      const response = await axios.put(this.endpoint)
+
+      this.isFollowedBy = true
+    },
+    async unfollow() {
+      const response = await axios.delete(this.endpoint)
+
+      this.isFollowedBy = false
     },
   },
 }
